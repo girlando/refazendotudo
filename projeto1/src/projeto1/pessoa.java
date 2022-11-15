86cf072d6598326b584305c0f4cf42e832d38314
Girlando dos santos <girlandosantoslima28@gmail.com>
2022-11-15 13:57:24 -0300

    adicoin

--- projeto1/src/projeto1/pessoa.java
+++ projeto1/src/projeto1/pessoa.java
@@ -5,5 +5,19 @@ public class pessoa {
 	public static void main(String[] args) {
 		System.out.println("meu programa");
 	}
+	private String nome;
+	private String sobrenome;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
 
 }
